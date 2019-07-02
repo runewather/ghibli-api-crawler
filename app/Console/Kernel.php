78ef98f819c2491a9714f0cbc8b4385890b1281f
Victo Principe Pessoa Borges <victoborges@tjrn.jus.br>
2019-07-02 11:06:26 -0300

    Schedule api:crawl command to run every 2 hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\ApiCrawl::class,
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('api:crawl')->cron('0 */2 * * *'); // every 2 hours
     }
 
     /**
